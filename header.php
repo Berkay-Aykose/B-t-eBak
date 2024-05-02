c859c05325903d52474ccae74dd6194080d45adc
Berkay Veysel Ayköse <98065247+Berkay-Aykose@users.noreply.github.com>
2024-05-02 03:38:41 +0300

    frontend update commit

--- header.php
+++ header.php
@@ -13,7 +13,7 @@ require_once 'netting/baglan.php';
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
 
-  <nav class="nav navbar-light bg-light border">
+  <nav class="nav navbar-light bg-light">
     <div class="container-fluid d-flex justify-content-center">
       <span style="font-size: 30px; font-weight:bold;">BütçeBak</span>
     </div>
